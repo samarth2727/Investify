cdcf569ff0cc492bfcb55f5983674099f32e3a0c
Samarth Math <samarthmath7@gmail.com>
2023-08-08 14:33:54 +0530

    updated

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,4 @@
 
-
 # Create your views here.
 from django.shortcuts import render
 
@@ -29,12 +28,13 @@ def investment_strategy(request):
             time_horizon = form.cleaned_data['time_horizon']
             investment_amount = form.cleaned_data['investment_amount']
             emergency_fund_percentage = form.cleaned_data['emergency_fund_percentage']
+
             # Convert investment_amount to float
             investment_amount = float(investment_amount)
 
             # Calculate the allocation percentages based on user input
-            stock_allocation = 0.5  # 50% for stocks
-            mutual_fund_allocation = 0.2  # 20% for mutual funds
+            stock_allocation = get_stock_allocation(risk_tolerance, time_horizon)
+            mutual_fund_allocation = get_mutual_fund_allocation(risk_tolerance, time_horizon)
             emergency_fund_allocation = emergency_fund_percentage / 100  # Convert to decimal
 
             # Calculate investment amounts for each category
@@ -42,19 +42,38 @@ def investment_strategy(request):
             mutual_fund_amount = mutual_fund_allocation * investment_amount
             emergency_fund_amount = emergency_fund_allocation * investment_amount
 
-            # Fetch some example investment options (replace with your actual data)
-            stocks = Stock.objects.all()[:5]
-            mutual_funds = MutualFund.objects.all()[:5]
+            # Replace this with your actual SIP suggestion logic
+            suggested_sips = [
+                {'name': 'SIP Option 1', 'type': 'Equity', 'amount': 100},
+                {'name': 'SIP Option 2', 'type': 'Debt', 'amount': 150}
+            ]
 
             return render(request, 'investment_strategy.html', {
                 'stock_amount': stock_amount,
                 'mutual_fund_amount': mutual_fund_amount,
                 'emergency_fund_amount': emergency_fund_amount,
-                'stocks': stocks,
-                'mutual_funds': mutual_funds,
+                'suggested_sips': suggested_sips,
             })
 
     else:
         form = InvestmentForm()
 
     return render(request, 'services.html', {'form': form})
+
+def get_stock_allocation(risk_tolerance, time_horizon):
+    # Calculate stock allocation based on risk tolerance and time horizon
+    if risk_tolerance == '1':
+        return 0.4 if time_horizon == '3' else 0.3
+    elif risk_tolerance == '2':
+        return 0.5 if time_horizon == '3' else 0.4
+    else:
+        return 0.6 if time_horizon == '3' else 0.5
+
+def get_mutual_fund_allocation(risk_tolerance, time_horizon):
+    # Calculate mutual fund allocation based on risk tolerance and time horizon
+    if risk_tolerance == '1':
+        return 0.3 if time_horizon == '3' else 0.4
+    elif risk_tolerance == '2':
+        return 0.3 if time_horizon == '3' else 0.3
+    else:
+        return 0.2 if time_horizon == '3' else 0.2
